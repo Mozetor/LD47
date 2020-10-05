b0a18d44effb612fd54cc6dbb862d50ecec0889d
Sh0ckwave <Fabian.niedermayer@gmx.at>
2020-10-05 11:01:59 +0200

    improve fix for visible lines on night/day mode by changing background colour, remove debug key kill wave outside unity editor

--- LD47/Assets/Scripts/UI/CameraController.cs
+++ LD47/Assets/Scripts/UI/CameraController.cs
@@ -1,10 +1,21 @@
-﻿using UnityEngine;
+﻿using Assets.WaveSpawner.Implementation;
+using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
     [SerializeField]
     [Tooltip("Speed of camera")]
     private float cameraSpeed = 20;
+    [Tooltip("Camera background color on day")]
+    public Color dayBackground;
+    [Tooltip("Camera background color on night")]
+    public Color nightBackground;
+
+    private void Awake() {
+        var spawner = FindObjectOfType<BuildBattleSpawner>();
+        spawner.AddOnBuildPhaseEnd(() => SetBackgroundColour(false));
+        spawner.AddOnBattlePhaseEnd(() => SetBackgroundColour(true));
+    }
 
     private void Update() {
         Vector3 pos = transform.position;
@@ -13,6 +24,11 @@ public class CameraController : MonoBehaviour {
         pos.y = Mathf.Clamp(pos.y + Input.GetAxis("Vertical") * Time.deltaTime * cameraSpeed, -25, 25);
 
         transform.position = pos;
+    }
+
+    private void SetBackgroundColour(bool light) {
+        Debug.Log("is called,light:" + light);
+        GetComponent<Camera>().backgroundColor = light ? dayBackground : nightBackground;
     }
 
 }
