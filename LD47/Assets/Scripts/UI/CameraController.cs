800ebff81ab2dcf442375b337bd2a017006d9216
Sh0ckwave <Fabian.niedermayer@gmx.at>
2020-10-05 11:32:41 +0200

    add camera deep scrolling, add cammera screen sidescrolling

--- LD47/Assets/Scripts/UI/CameraController.cs
+++ LD47/Assets/Scripts/UI/CameraController.cs
@@ -6,6 +6,14 @@ public class CameraController : MonoBehaviour {
     [SerializeField]
     [Tooltip("Speed of camera")]
     private float cameraSpeed = 20;
+    [Tooltip("Size of Sidescroll bar in pixels")]
+    public float mouseSideScrollBar = 50;
+    [Tooltip("speed of Sidescroll by mouse")]
+    public float mouseSidescrollSpeed = 0.5f;
+    [Tooltip("speed of Depthscroll")]
+    public float depthSidescrollSpeed = 0.5f;
+    [Tooltip("limit how far can be scrolled: x=close, y=far")]
+    public Vector2 depthSidescrollLimit;
     [Tooltip("Camera background color on day")]
     public Color dayBackground;
     [Tooltip("Camera background color on night")]
@@ -23,11 +31,35 @@ public class CameraController : MonoBehaviour {
         pos.x = Mathf.Clamp(pos.x + Input.GetAxis("Horizontal") * Time.deltaTime * cameraSpeed, -25, 25);
         pos.y = Mathf.Clamp(pos.y + Input.GetAxis("Vertical") * Time.deltaTime * cameraSpeed, -25, 25);
 
+        if (pos.z <= depthSidescrollLimit.y && Input.mouseScrollDelta.y > 0 || pos.z >= depthSidescrollLimit.x && Input.mouseScrollDelta.y < 0) {
+            pos.z += Input.mouseScrollDelta.y * depthSidescrollSpeed;
+        }
+
+        pos += ScreenSideScrolling();
+
+
         transform.position = pos;
     }
 
+    private Vector3 ScreenSideScrolling() {
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 scrollDirection = Vector2.zero;
+        if (mousePosition.x < mouseSideScrollBar) {
+            scrollDirection.x -= mouseSidescrollSpeed;
+        }
+        else if (mousePosition.x > Screen.width - mouseSideScrollBar) {
+            scrollDirection.x += mouseSidescrollSpeed;
+        }
+        if (mousePosition.y < mouseSideScrollBar) {
+            scrollDirection.y -= mouseSidescrollSpeed;
+        }
+        else if (mousePosition.y > Screen.height - mouseSideScrollBar) {
+            scrollDirection.y += mouseSidescrollSpeed;
+        }
+        return scrollDirection;
+    }
+
     private void SetBackgroundColour(bool light) {
-        Debug.Log("is called,light:" + light);
         GetComponent<Camera>().backgroundColor = light ? dayBackground : nightBackground;
     }
 
