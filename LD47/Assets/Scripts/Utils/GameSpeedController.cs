f29089bd91b824463bcf1df86ad576a12b8c0c79
Mozetor <maurice-zerressen@web.de>
2020-10-05 21:17:20 +0200

    Add game speed feature

---
+++ LD47/Assets/Scripts/Utils/GameSpeedController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Utils {
+
+    public class GameSpeedController : MonoBehaviour {
+
+        public Image speed1, speed2, speed3;
+
+        private void Awake() {
+            Time.timeScale = 1f;
+            UpdateUI();
+        }
+
+        public void SetGameSpeed(float speed) {
+            Time.timeScale = speed;
+            UpdateUI();
+        }
+
+        private void UpdateUI() {
+            speed1.color = Time.timeScale == 1 ? Color.green : Color.white;
+            speed2.color = Time.timeScale == 2 ? Color.green : Color.white;
+            speed3.color = Time.timeScale == 3 ? Color.green : Color.white;
+        }
+    }
+}
